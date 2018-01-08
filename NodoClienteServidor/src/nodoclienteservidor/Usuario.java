32d899f764d0cfcfde150933237c2c7d9ec03273
Marvian <marvianmontoya@hotmail.com>
2018-01-08 11:32:40 -0300

    Iniciando

---
+++ NodoClienteServidor/src/nodoclienteservidor/Usuario.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nodoclienteservidor;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author marvian
+ */
+public class Usuario implements Serializable {
+    
+    private String usuario;
+    private String clave;
+    private String direccionIP;
+    private long cantidadDescargas;
+    
+    public Usuario(String usuario, String clave, String correo, String direccionIP) {
+		super();
+		this.usuario = usuario;
+		this.clave = clave;
+		this.direccionIP = direccionIP;
+	}
+	
+	public Usuario(){
+		this.usuario = "";
+		this.clave = "";
+		this.direccionIP = "";
+		this.cantidadDescargas = 0;
+	}
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the clave
+     */
+    public String getClave() {
+        return clave;
+    }
+
+    /**
+     * @param clave the clave to set
+     */
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    /**
+     * @return the direccionIP
+     */
+    public String getDireccionIP() {
+        return direccionIP;
+    }
+
+    /**
+     * @param direccionIP the direccionIP to set
+     */
+    public void setDireccionIP(String direccionIP) {
+        this.direccionIP = direccionIP;
+    }
+
+    /**
+     * @return the cantidadDescargas
+     */
+    public long getCantidadDescargas() {
+        return cantidadDescargas;
+    }
+
+    /**
+     * @param cantidadDescargas the cantidadDescargas to set
+     */
+    public void setCantidadDescargas(long cantidadDescargas) {
+        this.cantidadDescargas = cantidadDescargas;
+    }
+    
+    
+}
