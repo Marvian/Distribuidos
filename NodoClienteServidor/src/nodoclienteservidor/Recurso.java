b400297a4d72e6c4ce07767dddbd1e5656443673
Marvian <marvianmontoya@hotmail.com>
2018-01-09 12:52:04 -0300

    Agregando Json para guardar los recursos

---
+++ NodoClienteServidor/src/nodoclienteservidor/Recurso.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nodoclienteservidor;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author marvian
+ */
+public class Recurso implements Serializable {
+    
+    private String nombre;
+    private String hashNombre;
+    private long cantidad;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the hashNombre
+     */
+    public String getHashNombre() {
+        return hashNombre;
+    }
+
+    /**
+     * @param hashNombre the hashNombre to set
+     */
+    public void setHashNombre(String hashNombre) {
+        this.hashNombre = hashNombre;
+    }
+
+    /**
+     * @return the cantidadDescargas
+     */
+    public long getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidadDescargas the cantidadDescargas to set
+     */
+    public void setCantidad(long cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+    
+}
